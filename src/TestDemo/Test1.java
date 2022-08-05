baa928b149cba927bd91ee941da8e9c549738682
Shriniket <Shriniket@Shree>
2022-08-05 16:42:16 +0530

    First commit.............

---
+++ src/TestDemo/Test1.java
@@ -0,0 +1,15 @@
+package TestDemo;
+
+public class Test1 {
+	public static void main(String[] args) {
+		
+		String hungry = "yes";
+		if(hungry == "yes") {
+			System.out.println("east something.......");
+		}
+		else {
+			System.out.println("Do Work.....");
+		}
+	}
+
+}
